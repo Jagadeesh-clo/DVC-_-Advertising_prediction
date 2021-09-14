db7e93383de2958ac6b2b8e2e533fd6fa60300ba
JAGADEESH R <jagadeeshabcinfo@gmail.com>
2021-09-14 21:54:26 +0530

    Completed

--- src/load_data.py
+++ src/load_data.py
@@ -10,10 +10,10 @@ def load_and_save(config_path):
     raw_data_path = config["load_data"]["raw_dataset_csv"]
     df.to_csv(raw_data_path, sep = ",", index=False, header=new_cols)
     #print(new_cols)
+
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parsed_args = args.parse_args()
     data = load_and_save(config_path=parsed_args.config)
-
 
